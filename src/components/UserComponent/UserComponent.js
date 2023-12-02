f903d06dcfa85b3ac0943579448b6def990dcca3
Ravi Raj Kumar <124041889+RaviRaj2703@users.noreply.github.com>
2023-12-02 15:24:11 +0530

    Initial Commit

---
+++ src/components/UserComponent/UserComponent.js
@@ -0,0 +1,92 @@
+import { useRef } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./UserComponent.module.css";
+
+const User = (props) => {
+  const { user, deleteUser, editUser, saveUser, selectOne } = props;
+
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const roleRef = useRef(null);
+
+  return (
+    <tr key={user.id} className={user.selected ? styles.selected : ""}>
+      <td>
+        <label for={`check-${user.id}`}>
+          <input
+            id={`check-${user.id}`}
+            type="checkbox"
+            data={`${user.selected}`}
+            onChange={() => selectOne(user.id)}
+            checked={user.selected}
+          ></input>
+        </label>
+      </td>
+      <td>
+        <input
+          className={user.edit ? styles.editable : styles.readOnly}
+          readOnly={!user.edit}
+          type="text"
+          ref={nameRef}
+          name="name"
+          defaultValue={user.name}
+        ></input>
+      </td>
+      <td>
+        <input
+          className={user.edit ? styles.editable : styles.readOnly}
+          readOnly={!user.edit}
+          type="email"
+          ref={emailRef}
+          name="email"
+          defaultValue={user.email}
+        />
+      </td>
+      <td>
+        <input
+          className={user.edit ? styles.editable : styles.readOnly}
+          readOnly={!user.edit}
+          type="text"
+          ref={roleRef}
+          name="role"
+          defaultValue={user.role}
+        />
+      </td>
+      <td className={styles.icons}>
+        {user.edit ? (
+          <button
+            onClick={() => saveUser(user.id, nameRef, emailRef, roleRef)}
+            className={`${styles.iconButton} ${styles.saveButton}`}
+          >
+            Save
+          </button>
+        ) : (
+          <button
+            onClick={() => editUser(user.id)}
+            className={`${styles.iconButton} ${styles.editButton}`}
+          >
+            Edit
+          </button>
+        )}
+
+        <button
+          onClick={() => deleteUser(user.id)}
+          className={`${styles.iconButton} ${styles.deleteButton}`}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+User.propTypes = {
+  user: PropTypes.object,
+  deleteUser: PropTypes.func,
+  editUser: PropTypes.func,
+  saveUser: PropTypes.func,
+  selectOne: PropTypes.func
+};
+
+export default User;
